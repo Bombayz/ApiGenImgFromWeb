f9071d3cb96bba748f4387647dcc8e3b3300a04b
Pobthorn Pimpa (Bombay) <p.pobthorn@kkumail.com>
2017-08-05 02:57:39 +0700

    [Edit] Add Parameter Page & Exception

--- index.php
+++ index.php
@@ -1,95 +1,90 @@
 <?php
-    // echo "Bay";
-    $url = 'https://wall.alphacoders.com/search.php?search=rem'; 
-  
-    // $data = "fn=login&test=1";
-    
-    /*$data = array(
-            'fn' => "login" 
-        );*/
-    
-    $i = []; 
-    $src = []; 
-    
-    try{
-        error_reporting(E_ERROR | E_PARSE);
+    // <---- Author: Bombay Pobthorn ------>
+    // <---- Date: 5/8/2017 ------>
 
-        $ch = curl_init();
-        curl_setopt( $ch, CURLOPT_URL, $url );
-        // curl_setopt( $ch, CURLOPT_POSTFIELDS, $data );
-        curl_setopt( $ch, CURLOPT_POST, true );
-        curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
-        $content = curl_exec( $ch );
-        curl_close($ch);
+    if($_GET['seacrh']){
+        $url = 'https://wall.alphacoders.com/search.php?search=';
+        $url .= $_GET['seacrh']; 
 
-
-        
-        // array_push($str,"เชียงใหม่");
-        // echo $str[0] ;
-        // echo strpos($content,"<img src=");
-
-        $doc = new DOMDocument();
-        $doc->loadHTML($content);    
-        $selector = new DOMXPath($doc);
-
-        $result = $selector->query('//div[@class="boxgrid"]/a');
-
-        // loop through all found items
-        foreach($result as $node) {
-            // echo "<p><a href=".$node->getAttribute('src').">".$node->getAttribute('src')."</a></p>";
-            
-            // echo "<p><a href=".$node->getAttribute('href')." >".$node->getAttribute('href')."</a></p>";
-            // echo "<br>";
-            array_push($i,$node->getAttribute('href'));
+        if($_GET['page']){
+            $url .= '&page='; 
+            $url .= $_GET['page']; 
         }
 
-        $result2 = $selector->query('//div[@class="boxgrid"]/a/img');
+        $i = []; 
+        $src = []; 
+    
+        try{
+            error_reporting(E_ERROR | E_PARSE);
 
-        // loop through all found items
-        foreach($result2 as $node) {
-            // echo "<p><a href=".$node->getAttribute('src').">".$node->getAttribute('src')."</a></p>";
+            // request html 
+            $ch = curl_init();
+            curl_setopt( $ch, CURLOPT_URL, $url );
+            curl_setopt( $ch, CURLOPT_POST, true );
+            curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+            $content = curl_exec( $ch );
+            curl_close( $ch );
+
+            // put html to DomDocument
+            $doc = new DOMDocument();
+            $doc->loadHTML( $content );    
+            $selector = new DOMXPath( $doc );
+
+            $result = $selector->query('//div[@class="boxgrid"]/a');
+
+            // loop through all found items
+            foreach( $result as $node ) {
+                array_push( $i,$node->getAttribute('href') );
+            }
+
+            $result2 = $selector->query('//div[@class="boxgrid"]/a/img');
+
+            // loop through all found items
+            foreach( $result2 as $node ) {
+                array_push($src,$node->getAttribute('src'));
+            }
+
+            $myData = array();
+            $data = array();
+
+            // loop put img & big img in json
+            for ($x=0; $x < count($i); $x++) { 
+                $json = array();
+                $json['id'] = $i[$x];
+                $json['src'] = $src[$x];
+                array_push($data,$json);
+            }
+
+            $myData['data'] = $data;
+
+            // check empty 
+            if( !empty( $myData['data'] ) ){
+                $find = $selector->query('//h1[@class="center title"]');
+                foreach( $find as $node ) {
+                    $myData['msg'] = "Found ".$node->nodeValue ;
+                }
+                
+            }else{
+                $myData['msg'] = "Not Found" ;
+            }
+
+            $myData['status'] = "ok";
+
+            // make header to json format
+            header('Content-Type: application/json');
+            echo json_encode($myData);
             
-            // echo "<p><a href=".$node->getAttribute('src')." >".$node->getAttribute('src')."</a></p>";
-            // echo "<br>";
-            array_push($src,$node->getAttribute('src'));
+        }catch(Exception $ex){
+            echo $ex;
         }
 
-        // print_r($i);
-        // print_r($src);
-
+    }else{
         $myData = array();
-        $data = array();
-
-        for ($x=0; $x < count($i); $x++) { 
-            $json = array();
-            $json['id'] = $i[$x];
-            $json['src'] = $src[$x];
-
-            // echo $i[$x];
-            // echo $src[$x];
-            array_push($data,$json);
-        }
-
-        $myData['data'] = $data;
-
-
+        $myData['msg'] = "Nothing to do";
         $myData['status'] = "ok";
-
         header('Content-Type: application/json');
         echo json_encode($myData);
-
-        
-
-
-        // print_r($content);
-        // var_dump($content);
-
-
-
-        // echo($content);
-        
-    }catch(Exception $ex){
-    
-        echo $ex;
     }
+
+    
